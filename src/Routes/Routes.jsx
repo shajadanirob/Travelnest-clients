589eabb9782e7deca9317d551fe42197362caa8e
hossainnirob <shajadanirob@gmail.com>
2023-11-07 12:56:39 +0600

    user Lottie anmition in Error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -16,6 +16,7 @@ import ServicesDetails from "../Pages/Services/ServicesDetals/ServicesDetails";
 import Modal from "../Pages/Services/ServicesDetals/Modal";
 
 import SingleServices from "../Pages/Services/SingleServices";
+import ErrorPage from "../Components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
@@ -24,15 +25,18 @@ const router = createBrowserRouter([
         children:[
             {
                 path:'/',
+                errorElement:<ErrorPage></ErrorPage>,
                 element:<Home></Home>
             },
             {
                 path:'/TravelNest/services',
+                errorElement:<ErrorPage></ErrorPage>,
                 element:<Services></Services>,
                 loader: () => fetch('http://localhost:5000/services')
             },
             {
                 path:'/services/:id',
+                errorElement:<ErrorPage></ErrorPage>,
                 element:<PrivetRoute>
                     <ServicesDetails></ServicesDetails>
                 </PrivetRoute>,
@@ -41,45 +45,53 @@ const router = createBrowserRouter([
             },
             {
                 path:'/services/:id',
+                errorElement:<ErrorPage></ErrorPage>,
                 element:<Modal></Modal>,
                 loader:({params}) => fetch(`http://localhost:5000/services/${params.id}`)
 
             },
             {
                 path:'/TravelNest/update/:id',
+                errorElement:<ErrorPage></ErrorPage>,
                 element:<UpdateServices></UpdateServices>,
                 loader:({params}) => fetch(`http://localhost:5000/update/${params.id}`)
             },
             {
                 path:'/TravelNest/dashboard',
+                errorElement:<ErrorPage></ErrorPage>,
                 element:<DashBoard></DashBoard>,
                 children:[
                     {
                         path:'/TravelNest/dashboard',
+                        errorElement:<ErrorPage></ErrorPage>,
                         element:<PrivetRoute>
                             <AddServices></AddServices>
                         </PrivetRoute>
                     },
                     {
                         path:'/TravelNest/dashboard/updated',
+                        errorElement:<ErrorPage></ErrorPage>,
                         element:<PrivetRoute>
                             <ManageService></ManageService>
                         </PrivetRoute>
                     },
                     {
                         path:'/TravelNest/dashboard/schedules',
+                        errorElement:<ErrorPage></ErrorPage>,
                         element:<PrivetRoute>
                             <Schedules></Schedules>
                         </PrivetRoute>
                     },
                     {
                        path:'/TravelNest/dashboard/bookings',
+                       errorElement:<ErrorPage></ErrorPage>,
                        element:<PrivetRoute>
                         <MyBookings></MyBookings>
                        </PrivetRoute> 
                     },
                     {
                         path:'/TravelNest/dashboard/pending',
+                        errorElement:<ErrorPage></ErrorPage>,
                         element:<PrivetRoute>
                             <MyPendingWork></MyPendingWork>
                         </PrivetRoute>,
@@ -89,10 +101,12 @@ const router = createBrowserRouter([
             },
             {
                 path:'/TravelNest/login',
+                errorElement:<ErrorPage></ErrorPage>,
                 element:<Login></Login>
             },
             {
                 path:'/TravelNest/register',
+                errorElement:<ErrorPage></ErrorPage>,
                 element:<Register></Register>
             },
            
